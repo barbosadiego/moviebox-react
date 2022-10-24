a3b5be24c53bd7fb3a2fa28904a7ecec5502fec9
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-24 14:34:50 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.scss';
+import Logo from '../img/Logo.svg';
 
-function Header(props) {
+function Header() {
   return (
-    <header>
-      <div className="container">
-        <p>header</p>
-      </div>
+    <header className="header">
+      <nav className="container flex">
+        <div>
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
+        </div>
+
+        <input className='header__input' type="text" placeholder='What do you want to watch?'/>
+
+        <div className="header__menu flex">
+          <h3>Sing in</h3>
+          <div className="btn">
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
