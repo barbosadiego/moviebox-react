bd66358d490f76a30e132b7688900b209b77af7e
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-29 14:51:17 -0300

    loading item

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//css
+import './Loading.scss';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
